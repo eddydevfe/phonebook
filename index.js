cffc0351aeac934111f8e49b5447eda51d6414ce
eddydevfe <pedro@DESKTOP-QADQCE1.>
2024-06-10 11:49:11 -0300

    prepare for deployment

---
+++ index.js
@@ -0,0 +1,103 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+app.use(express.json())
+app.use(cors())
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get("/api/persons", (request, response) => {
+    response.json(persons)
+})
+
+app.get("/info", (request, response) => {
+    response.send(`
+            <span>Phonebook has info for ${persons.length}</span>
+            <br/>
+            <span>${new Date}</span>
+        `)
+})
+
+app.get("/api/persons/:id", (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    
+    if (person) {
+        response.json(person)
+    } else {
+        response.statusMessage = "Person was not found."
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+  
+    response.status(204).end()
+})
+
+const generateId = () => {
+    return Math.floor(Math.random() * 9000000000) + 1000000000; 
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'name missing'
+        })
+    }
+
+    if (!body.number) {
+        return response.status(400).json({
+            error: 'number missing'
+        })
+    }
+
+    const existing = persons.find(person => person.name === body.name)
+    if (existing) {
+        return response.status(409).json({
+            error: 'name must be unique'
+        })
+    }
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: Number(body.number),
+    }
+
+    // Just return a new copy of the array with the added person.
+    let newPersons = persons.concat(person)
+
+    response.json(person)
+})
+
+const PORT = 3000; // 3001 is already being used, I'm not sure why since the other app is closed
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
